6b3957b8eccc0eae2a02f39af7c22bd46b342e99
CheFraHer19 <chelseabagels@gmail.com>
2020-10-14 21:58:04 -0500

    Problema 1 completo

--- JS/problema1.js
+++ JS/problema1.js
@@ -2,4 +2,4 @@ var radio = prompt ("Escribe el radio: ");
 var pi = 3.1416;
 var volumen = 4/3.0 * pi * radio * radio * radio;
 alert("El volumen es: " + volumen);
-document.write("El volumen es: " + volumen);
+document.write("<p> El volumen es: " + volumen + "</p>");
